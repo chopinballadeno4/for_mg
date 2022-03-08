625c7908953a21beffa8822bb2ee266475883777
BaeSungMin <qo98333@naver.com>
2022-03-08 10:46:28 +0900

    back button function

--- src/routes/stock.js
+++ src/routes/stock.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../css/stockcss.css";
 
 function Stock() {
+    const navigate = useNavigate();
+    
+    const onClick = (event) => {
+        navigate(-1);
+    }
+
     return(
         <div className="TD">
             <div>
                 <button
                 className="Tap_button"
-                type="button">
+                type="button"
+                onClick={onClick}
+                >
                 <img src="/image/back_button_arrow.png" alt="back_button"/>
                 </button>
             </div>
